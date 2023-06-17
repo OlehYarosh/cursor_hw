61702b466fc1d0a94b36370bf696d9051018f256
shoria <yarosh.oleg07@gmail.com>
2023-06-17 16:48:50 +0300

    7-11 task

--- hw3/main.js
+++ hw3/main.js
@@ -1,4 +1,4 @@
-function task1(number) {
+function getMaxDigit(number) {
     const digits = String(number).split('');
 
     let maxNumber = digits[0];
@@ -11,20 +11,20 @@ function task1(number) {
     return maxNumber;
 }
 
-function task2(number, step){
+function pow(number, step){
     let result = number;
     for (let i = 1; i < step; i++){
-        result = result * number;
+        result *= number;
     }
     return result;
 }
 
-function task3(word) {
+function validateWord(word) {
     update_word = word[0].toUpperCase() + word.slice(1).toLowerCase();
     return update_word;
 }
 
-function task4(sum, tax1, tax2) {
+function salaryWithoutTax(sum, tax1, tax2) {
     let tax;
     tax = tax1 + tax2;
     taxWithSum = (sum*tax)/100;
@@ -32,12 +32,12 @@ function task4(sum, tax1, tax2) {
     return lastSum
 }
 
-function task5(firstNumber, lastNumber) {
+function getRandomNumber(firstNumber, lastNumber) {
     let number = Math.round((lastNumber - firstNumber) * Math.random(firstNumber, lastNumber) + 1);
     return number
 }
 
-function task6(anyWord, letter) {
+function countLetter(anyWord, letter) {
     amount = 0;
     anyWord = anyWord.toLowerCase();
     letter = letter.toLowerCase();
@@ -49,12 +49,50 @@ function task6(anyWord, letter) {
     return amount;
 }
 
+function convertCurrency(value) {
+    if (value.includes('$')) {
+        return Math.round(parseFloat(value) * 36.9) + " UAH"
+    } else if (value.toUpperCase().includes('UAH')) {
+        return Math.round(parseFloat(value) * 0.02710028) + " $"
+    } else {
+        return "Ми конвертуємо тільки UAH -> $, або $ -> UAH"
+    }
+}
+
+function getRandomPassword(passwordLength = 8) {
+    password = Math.random()*10
+    result = String(password).slice(-passwordLength)
+    return result
+}
+
+function deleteLetters(sentence, lett) {
+    const newSentence = sentence.replaceAll(lett, '');
+    return newSentence
+}
+
+function isPalyndrom(word) {
+    let userWord = word.toLowerCase().replaceAll(' ', '');
+    let reverseWord = "";
+    for (let i = userWord.length - 1; i >= 0; i--){
+        reverseWord += userWord[i];
+    }
+    if (userWord === reverseWord){
+        return true
+    } else {
+        return false
+    }
+}
 
 
-console.log(`Функція №1: результат числа <23563> = ${task1(23563)};
-Функція №2: результат виразу <4^4> = ${task2(4, 4)};
-Функція №3: результат слова <cUrSoR> = ${task3("cUrSoR")};
-Функція №4: результат суми податків від зарплати = ${task4(17643, 10, 5.9)};
-Функція №5: результат діапазону між числами <1-100> = ${task5(1, 100)};
-Функція №6: результат кількості букв <L> у слові <parallelogram> = ${task6("parallelogram", "L")}
+console.log(`
+Функція №1: результат числа <23563> = ${getMaxDigit(23563)};
+Функція №2: результат виразу <4^4> = ${pow(4, 4)};
+Функція №3: результат слова <cUrSoR> = ${validateWord("cUrSoR")};
+Функція №4: результат суми податків від зарплати = ${salaryWithoutTax(17643, 10, 5.9)};
+Функція №5: результат діапазону між числами <1-100> = ${getRandomNumber(1, 100)};
+Функція №6: результат кількості букв <L> у слові <parallelogram> = ${countLetter("parallelogram", "L")};
+Функція №7-8: результат конвертації $ -> UAH = ${convertCurrency("100$")}, та UAH -> $ = ${convertCurrency("3690 uAh")};
+Функція №9: результат згенерованого паролю в довжину по замовчуванням: ${getRandomPassword()};
+Функція №10: результат видалення усіх букв <a> з речення <ababahalamaha>: ${deleteLetters('ababahalamaha', 'a')};
+Функція №11: результат перевірки слова/речення чи воно є паліндромом <Я несу гусеня>: ${isPalyndrom('Я несу гусеня')}.
 `)
