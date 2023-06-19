4477d2398750dc4cdaabb4c51d3b685a57ab87c3
shoria <yarosh.oleg07@gmail.com>
2023-06-19 18:53:57 +0300

    update HW#2

--- hw2/main.js
+++ hw2/main.js
@@ -1,25 +1,22 @@
 let firstNumberN = Number(prompt("Введіть ціле число N:"));
 let secondNumberM = Number(prompt("Введіть ціле число M:"));
 
-while (!Number.isInteger(firstNumberN)){
-    firstNumberN = Number(prompt("Число N було не ціле, будь-ласка напишіть ціле число:", firstNumberN))
+while (!Number.isInteger(firstNumberN) || firstNumberN <= 0 || isNaN(firstNumberN)){
+    firstNumberN = Number(prompt("Число N не було коректно введено:", firstNumberN))
 }
-while (!Number.isInteger(secondNumberM)){
-    secondNumberM = Number(prompt("Число M було не ціле, будь-ласка напишіть ціле число:", secondNumberM))
+while (!Number.isInteger(secondNumberM) || secondNumberM <= firstNumberN || isNaN(secondNumberM)){
+    secondNumberM = Number(prompt("Число M не було коректно введено:", secondNumberM))
 }
-
-const numbers = `Число N: ${firstNumberN} ---> Число M: ${secondNumberM}`;
-console.log(numbers)
 
 let sum = 0;
-if (confirm("Чи пропускати парні?") === true) {
-    for (let i = firstNumberN; i<=secondNumberM; i++) {
-        if((i % 2) !== 0){
-            sum += i;
-        }
-    } console.log("Сума усіх непарних чисел:", sum)
-} else {
-    for (let i = firstNumberN; i<=secondNumberM; i++) {
-        sum += i;
-    } console.log("Сума усіх чисел :", sum)
+let skip = confirm("Чи пропускати парні?");
+for (let i = firstNumberN; i <= secondNumberM; i++) {
+  if (skip && i % 2 === 0) {
+    continue;
+  }
+  sum += i;
 }
+
+console.log(`Пропускаємо парні числа? ${skip}
+Результат суми чисел ${firstNumberN} і ${secondNumberM}: ${sum}
+`)
